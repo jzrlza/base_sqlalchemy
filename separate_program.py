02d81ec7a81c8dc078064d59b76c3c8f92f165b9
jzrlza <parinvut12345@gmail.com>
2023-12-08 09:46:34 +0700

    retime

--- separate_program.py
+++ separate_program.py
@@ -1,23 +1,33 @@
 import db_connect
 import random
 from datetime import datetime, timedelta
+from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 import tkinter as tk
 tk_app = tk.Tk()
 
 db = db_connect.SessionLocal()
 
+tk_app.title('Test')
+
 def add_task() :
 	now = datetime.now()
-	date_time = now.strftime("%Y-%m-%dT%H:%M:%S")
+	date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 	the_task_model = db_connect.Tasks()
 	the_task_model.name = "test"+str(random.randint(1, 1000))
 	the_task_model.date_created = str(date_time)
 	db.add(the_task_model)
 	db.commit()
 
-tk_app.title('Test')
-button = tk.Button(tk_app, text='Add Task', width=25, command=add_task)
-button.pack()
+def get_tasks() :
+	raw_sql = text('SELECT * FROM tasks WHERE is_deleted = :deletion_bool ORDER BY id')
+	result = db.execute(raw_sql, {"deletion_bool": False}).fetchall()
+	print(result)
+
+button1 = tk.Button(tk_app, text='Add Task', width=25, command=add_task)
+button1.pack()
+button2 = tk.Button(tk_app, text='Display Tasks', width=25, command=get_tasks)
+button2.pack()
 
 tk_app.mainloop()
